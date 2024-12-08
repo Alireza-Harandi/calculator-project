3549143c19fada74e9a2a832d6662d6ed7dc471e
Alireza <aliharadi10@gmail.com>
2024-12-09 03:15:40 +0330

    AddOperand function in OperandController and add PI, NE

--- Calculatorr/src/main/java/com/example/calculatorr/controller/OperandController.java
+++ Calculatorr/src/main/java/com/example/calculatorr/controller/OperandController.java
@@ -13,4 +13,15 @@ public class OperandController {
             operandController = new OperandController();
         return operandController;
     }
+
+    public DoublyLinkedList<Operand> operands = new DoublyLinkedList<>();
+
+    static {
+        OperandController.getOperandController().operands.add(new Operand("PI", 3.14159));
+        OperandController.getOperandController().operands.add(new Operand("EN", 2.71828));
+    }
+
+    public void addOperand(Operand operand) {
+        operands.add(operand);
+    }
 }
