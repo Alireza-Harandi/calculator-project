434128c336cc2649b9954c830b097838805a14e2
Alireza <aliharadi10@gmail.com>
2024-12-09 02:56:09 +0330

    EquationException class

---
+++ Calculatorr/src/main/java/com/example/calculatorr/exception/EquationException.java
@@ -0,0 +1,7 @@
+package com.example.calculatorr.exception;
+
+public class EquationException extends RuntimeException {
+    public EquationException(String message) {
+        super(message);
+    }
+}
