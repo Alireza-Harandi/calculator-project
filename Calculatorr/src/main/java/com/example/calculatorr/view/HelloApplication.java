0c04fafeef7f6f4605956b581fec724a2c893b40
Alireza <aliharadi10@gmail.com>
2024-12-09 02:44:56 +0330

    start

---
+++ Calculatorr/src/main/java/com/example/calculatorr/view/HelloApplication.java
@@ -0,0 +1,7 @@
+package com.example.calculatorr.view;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Calculatorr/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.calculatorr {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.calculatorr.view to javafx.fxml;
+    exports com.example.calculatorr.view;
+}
