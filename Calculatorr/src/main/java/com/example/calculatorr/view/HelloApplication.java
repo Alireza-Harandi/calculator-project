a2689bb6dbbc82d5fb47e373ab87063ecfcb24b7
Alireza <aliharadi10@gmail.com>
2024-12-09 04:26:46 +0330

    set input and output in console to show

--- Calculatorr/src/main/java/com/example/calculatorr/view/HelloApplication.java
+++ Calculatorr/src/main/java/com/example/calculatorr/view/HelloApplication.java
@@ -1,7 +1,20 @@
 package com.example.calculatorr.view;
 
+import com.example.calculatorr.controller.CalculatorController;
+
+import java.util.Scanner;
+
 public class HelloApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        String[] inputs = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            inputs[i] = sc.nextLine();
+        }
+
+        System.out.println(CalculatorController.getCalculatorController().calculate(n, inputs));
     }
 }
